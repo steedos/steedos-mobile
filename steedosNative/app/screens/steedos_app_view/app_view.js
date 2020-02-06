f90e5135c39e464b800732392fb35c7f898bf3d1
baozhoutao <baozhoutao@hotoa.com>
2020-02-06 17:48:08 +0800

    app详细页面显示“加载中”

--- steedosNative/app/screens/steedos_app_view/app_view.js
+++ steedosNative/app/screens/steedos_app_view/app_view.js
@@ -3,7 +3,7 @@
 
 import React, {PureComponent} from 'react';
 import {
-    View,Text
+    View,Text, StyleSheet
 } from 'react-native';
 import { WebView } from 'react-native-webview';
 import {intlShape, injectIntl} from 'react-intl';
@@ -11,6 +11,27 @@ import {Navigation} from 'react-native-navigation';
 import { dismissModal } from 'app/actions/navigation'
 
 import { getUserId,getAuthToken } from '../../utils/accounts'
+
+class WebLoadingView extends PureComponent {
+
+    render() {
+        return (
+            <View style={{flex:1,justifyContent:'center',
+        alignItems:'center'}}>
+                <Text style={styles.loadingText}>
+                    页面正在加载...
+                </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    loadingText: {
+        color: '#8a8a8a',
+        fontSize: 16
+    }
+})
 
 class AppView extends PureComponent {
 
@@ -54,9 +75,11 @@ class AppView extends PureComponent {
         let uri = `http://192.168.3.2:5000/api/setup/sso/${app?._id}?X-User-Id=${this.userId}&X-Auth-Token=${this.token}`
         console.log('app view uri', uri);
         return (
-            <WebView
-        source={{ uri }}
-      />
+            <WebView 
+                    source={{ uri }}
+                    startInLoadingState = {true}
+                    renderLoading = {()=>{return <WebLoadingView/>}}
+                />
         );
     }
 }
